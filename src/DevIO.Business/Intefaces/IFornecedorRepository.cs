e1ccb71a158a8999756a1edef0e249e5d8e011d4
Nathan Justino de Oliveira <nathan.oliveira@bgmrodotec.com.br>
2020-06-23 23:09:08 -0300

    Adicionado estrategia de mapeamento e repositorio e conexao com a base de dados

---
+++ src/DevIO.Business/Intefaces/IFornecedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Intefaces
+{
+    public interface IFornecedorRepository
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+    }
+}
---
+++ src/DevIO.Business/Intefaces/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Intefaces
+{
+    public interface IProdutoRepository
+    {
+        Task<Produto> ObterProdutoFornecedor(Guid id);
+        Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+    }
+}
