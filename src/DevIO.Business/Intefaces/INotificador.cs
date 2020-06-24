618d2fdc6b51a50d567faeaded8b03a9445d44cd
Nathan Justino de Oliveira <nathan.oliveira@bgmrodotec.com.br>
2020-06-23 22:29:56 -0300

    Adicionado estrategia de notificacao

---
+++ src/DevIO.Business/Intefaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using DevIO.Business.Notificacoes;
+using System.Collections.Generic;
+
+namespace DevIO.Business.Intefaces
+{
+    public interface INotificador
+    {
+        void Handle(Notificacao notificacao);
+        List<Notificacao> ObterNotificacoes();
+        bool TemNotificacao();
+    }
+}
